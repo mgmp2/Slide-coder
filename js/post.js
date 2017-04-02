62dd64f794432a60082e4467aceab75135d24346
Miriam Mendoza Pilco <miriammp1997@gmail.com>
2017-04-02 02:40:45 -0500

    ready createPanel

--- js/post.js
+++ js/post.js
@@ -1,2 +1,34 @@
 // document.getElementsByClassName('post-content).classList.remove("active");
 // document.getElementsByClassName('post-content).classList.toggle("active");
+
+var share = document.getElementById("post-content");
+document.getElementById("publicar").addEventListener("click", function(e)
+{
+  e.preventDefault();
+  var area = document.getElementById("area").value;
+  var user = document.getElementById("user").value;
+  if(user && area){
+    share.classList.add('active');
+    createPanel(document.createTextNode(user), document.createTextNode(area));
+  } else{
+   alert("Don't exist content");
+}
+});
+
+function createPanel(user, area){
+  var cdf = document.createDocumentFragment();
+  var firstChild = share.firstElementChild;
+  var shareContent = document.createElement("div");
+  var boxArea = document.createElement("span");
+  var boxUser = document.createElement("p");
+  shareContent.classList.add("div-share");
+
+  boxArea.appendChild(area);
+  boxUser.appendChild(user);
+  cdf.appendChild(boxArea);
+  cdf.appendChild(boxUser);
+  shareContent.appendChild(cdf);
+  share.appendChild(shareContent);
+   share.insertBefore(shareContent, firstChild);
+
+}
